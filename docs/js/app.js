8f43d045ad9f5f301982a0f9139a665600d297d0
Diegiwg <diegiwg@gmail.com>
2022-11-06 20:55:41 -0300

    tentando upar o site para o gitpage

---
+++ docs/js/app.js
@@ -0,0 +1,42 @@
+import { dados_jogos } from "./calendar.service.js";
+
+class App {
+    node = document.querySelector("#app");
+    top_bar = this.node.querySelector("#top-bar");
+    cards = this.node.querySelector("#cards");
+
+    new_card(info) {
+        let card = document.createElement("div");
+        card.className = "card";
+        // gerar todos os times do card
+        let jogos_html = "";
+        info.jogos.forEach((jogo) => {
+            jogos_html += `
+                <c-box-times>
+                    <img src="./static/icon/icon=${jogo.time_1}.svg" alt="icon ${jogo.time_1}" />
+                        ${jogo.horario}
+                    <img src="./static/icon/icon=${jogo.time_2}.svg" alt="icon ${jogo.time_2}" />
+                </c-box-times>
+            `;
+        });
+        //
+        let html = `
+            <ul>
+                <c-box-data>
+                    <c-data>${info.data}</c-data>
+                    <c-dia-semana>${info.dia}</c-dia-semana>
+                </c-box-data>
+                ${jogos_html}
+            </ul>
+        `;
+        //
+        card.innerHTML = html;
+        this.cards.appendChild(card);
+    }
+}
+
+const app = new App();
+dados_jogos.forEach((card) => {
+    console.log(card);
+    app.new_card(card);
+});
