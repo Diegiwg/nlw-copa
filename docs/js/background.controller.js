96ce70762950585691f0b58743b917fe8803f2a1
Diegiwg <diegiwg@gmail.com>
2022-11-06 21:53:32 -0300

    + correções de links (queria automatizar isso) ><

--- docs/js/background.controller.js
+++ docs/js/background.controller.js
@@ -2,8 +2,5 @@ function set_bg_color() {
     const background = ["bg-blue", "bg-green", "bg-yellow"];
     let user_bg = background[Math.floor(Math.random() * background.length)];
     document.body.classList = [user_bg];
-
-    setTimeout(() => {
-        document.body.hidden = false;
-    }, 1000);
+    document.body.hidden = false;
 }
